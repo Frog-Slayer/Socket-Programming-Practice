f554ad96fa599c304906b5d3a83e4b95345f965d
frog-slayer <pj0642@gmail.com>
2024-08-03 21:20:06 +0900

    implement one-time TCP server without while(true) loop

---
+++ Java-Socket-Practice/src/tcp/OnetimeTCPServer.java
@@ -0,0 +1,37 @@
+package tcp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class OnetimeTCPServer {
+    public void run() {
+        try (ServerSocket serverSocket = new ServerSocket(12000) ){
+            Socket connectionSocket = serverSocket.accept();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));																	// 데이터를 읽어옴
+
+            String input = reader.readLine();
+
+            SocketAddress address = connectionSocket.getRemoteSocketAddress();
+            int port = connectionSocket.getPort();
+
+            System.out.println("[Server] Received packet from " + address + ":" + port + ". data: " + input);
+
+            PrintWriter writer = new PrintWriter(new OutputStreamWriter(connectionSocket.getOutputStream())); // 소켓으로 데이터를
+
+            writer.println(input.toUpperCase());
+            writer.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        new OnetimeTCPServer().run();
+    }
+}
--- Java-Socket-Practice/src/udp/UDPClient.java
+++ Java-Socket-Practice/src/udp/UDPClient.java
@@ -2,9 +2,7 @@ package udp;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
+import java.net.*;
 
 public class UDPClient {
     public static String serverHost = "localhost";
@@ -22,9 +20,9 @@ public class UDPClient {
 
     public void run() {
 
-        try (DatagramSocket socket = new DatagramSocket(); BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-
+        try (DatagramSocket socket = new DatagramSocket(null); BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             InetAddress address = InetAddress.getByName(serverHost);
+            socket.bind((SocketAddress) new InetSocketAddress((InetAddress) null, 41210));
 
             String input = br.readLine();
             DatagramPacket outPacket = new DatagramPacket(input.getBytes(), input.length(), address, serverPort);
